764264837e0f82a522e2252013be2503c3b34855
Fabio Sanchez <holom.dev@gmail.com>
2023-02-26 22:52:00 -0600

    selecting by text

---
+++ cypress/e2e/basics.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+
+describe('tasks page', () => {
+  it('should render the main image', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('.main-header img')
+  })
+})
