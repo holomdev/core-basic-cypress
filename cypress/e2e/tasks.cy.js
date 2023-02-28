53bb58a302fdf6dd1331cb40c64af21b1ce5f928
Fabio Sanchez <holom.dev@gmail.com>
2023-02-27 23:47:17 -0600

    time for more queries

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -47,4 +47,24 @@ describe('tasks management', () => {
     cy.get('#filter').select('all')
     cy.get('.task').should('have.length', 1)
   })
+
+  it('should add multiple task', () => {
+    cy.visit('http://localhost:5173/')
+    cy.contains('Add Task').click()
+    cy.get('#title').type('Task 1')
+    cy.get('#summary').type('First Task')
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.task').should('have.length', 1)
+
+
+    cy.contains('Add Task').click()
+    cy.get('#title').type('Task 2')
+    cy.get('#summary').type('Second Task')
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.task').should('have.length', 2)
+
+    cy.get('.task').eq(0).contains('First Task')
+    cy.get('.task').eq(1).contains('Second Task')
+
+  })
 })
