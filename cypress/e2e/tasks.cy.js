8d8cd3e0f2fedefe02778b3247691ca0ba1b9ab3
holomdev <holom.dev@gmail.com>
2023-02-27 18:03:30 -0600

    selecting dropdown values

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -31,4 +31,20 @@ describe('tasks management', () => {
     cy.get('.modal').contains('Add Task').click()
     cy.contains('Please provide values')
   })
+
+  it('should filter task', () => {
+    cy.visit('http://localhost:5173/')
+    cy.contains('Add Task').click()
+    cy.get('#title').type('New task')
+    cy.get('#summary').type('Some Description')
+    cy.get('#category').select('urgent')
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.task').should('have.length', 1)
+    cy.get('#filter').select('moderate')
+    cy.get('.task').should('have.length', 0)
+    cy.get('#filter').select('urgent')
+    cy.get('.task').should('have.length', 1)
+    cy.get('#filter').select('all')
+    cy.get('.task').should('have.length', 1)
+  })
 })
